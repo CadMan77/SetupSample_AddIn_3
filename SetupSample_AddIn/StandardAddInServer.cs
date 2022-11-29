4c3e929d6e8a154cc8e7291616acad055266fe8a
cadman77 <cadman77@yandex.ru>
2022-11-29 15:07:04 +0300

    Add project files.

---
+++ SetupSample_AddIn/StandardAddInServer.cs
@@ -0,0 +1,79 @@
+using Inventor;
+using Microsoft.Win32;
+using System;
+using System.Runtime.InteropServices;
+
+namespace SetupSample_AddIn
+{
+    /// <summary>
+    /// This is the primary AddIn Server class that implements the ApplicationAddInServer interface
+    /// that all Inventor AddIns are required to implement. The communication between Inventor and
+    /// the AddIn is via the methods on this interface.
+    /// </summary>
+    [GuidAttribute("34b4bb25-4389-4ddc-bf88-219f8ee5d45c")]
+    public class StandardAddInServer : Inventor.ApplicationAddInServer
+    {
+
+        // Inventor application object.
+        private Inventor.Application inventor;
+
+        public StandardAddInServer()
+        {
+        }
+
+        #region ApplicationAddInServer Members
+
+        public void Activate(Inventor.ApplicationAddInSite addInSiteObject, bool firstTime)
+        {
+            // This method is called by Inventor when it loads the addin.
+            // The AddInSiteObject provides access to the Inventor Application object.
+            // The FirstTime flag indicates if the addin is loaded for the first time.
+
+            // Initialize AddIn members.
+            inventor = addInSiteObject.Application;
+
+            // TODO: Add ApplicationAddInServer.Activate implementation.
+            // e.g. event initialization, command creation etc.
+
+            inventor.CommandManager.PromptMessage("Sample addin loaded", 0);
+        }
+
+        public void Deactivate()
+        {
+            // This method is called by Inventor when the AddIn is unloaded.
+            // The AddIn will be unloaded either manually by the user or
+            // when the Inventor session is terminated
+
+            // TODO: Add ApplicationAddInServer.Deactivate implementation
+            inventor.CommandManager.PromptMessage("Sample addin unloading...", 0);
+            // Release objects.
+            inventor = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        public void ExecuteCommand(int commandID)
+        {
+            // Note:this method is now obsolete, you should use the 
+            // ControlDefinition functionality for implementing commands.
+        }
+
+        public object Automation
+        {
+            // This property is provided to allow the AddIn to expose an API 
+            // of its own to other programs. Typically, this  would be done by
+            // implementing the AddIn's API interface in a class and returning 
+            // that class object through this property.
+
+            get
+            {
+                // TODO: Add ApplicationAddInServer.Automation getter implementation
+                return null;
+            }
+        }
+
+        #endregion
+
+    }
+}
